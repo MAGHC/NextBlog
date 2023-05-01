022141ced22fa162af8662a89ef08bbe98f4e1d0
MAGHC <windowsapple95@gmail.com>
2023-05-01 22:18:38 +0900

    feat : footer 마크업 , 스타일

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,7 @@
+import style from './Footer.module.css';
+
+const Footer = () => {
+  return <footer className={style.footer}>Front-End developer | @2023 All right reserved</footer>;
+};
+
+export default Footer;
