bd93569ff7345de9dc974510d910ed577fbf5fc7
MAGHC <windowsapple95@gmail.com>
2023-05-03 19:57:43 +0900

    service : getAllposts 함수 작성

---
+++ src/service/posts.ts
@@ -0,0 +1,17 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+
+export type Post = {
+  title: string;
+  description: string;
+  date: Date;
+  path: string;
+};
+
+export async function getAllPosts(): Promise<Post[]> {
+  const filePath = path.join(process.cwd(), 'data', 'blogPosts', 'blogPosts.json');
+
+  return readFile(filePath, 'utf-8')
+    .then<Post[]>(JSON.parse)
+    .then((posts) => posts.sort((a, b) => (a.date > b.date ? -1 : 1)));
+}
