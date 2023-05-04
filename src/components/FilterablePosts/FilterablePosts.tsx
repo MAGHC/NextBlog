ba81aa75024c57cd7d87b08d8624f2bc671dad98
MAGHC <windowsapple95@gmail.com>
2023-05-04 21:42:50 +0900

    feat : 카테고리별 filterable 컴포넌트 생성 , 기능구현

---
+++ src/components/FilterablePosts/FilterablePosts.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { Post } from '@/service/posts';
+import style from './FilterablePosts.module.css';
+
+import GridWrapper from '../GridWrapper/GridWrapper';
+import Categories from '../Categories/Categories';
+import { useState } from 'react';
+
+type Props = {
+  posts: Post[];
+  categories: string[];
+};
+
+const ALL_POSTS = 'ALL';
+
+const FilterablePosts = ({ posts, categories }: Props) => {
+  const [filterableCategories, setFilterableCategories] = useState(ALL_POSTS);
+  const filteredPosts =
+    filterableCategories === ALL_POSTS
+      ? posts
+      : posts.filter((post) => post.category.toUpperCase() === filterableCategories);
+  return (
+    <section className={style.Wrapper}>
+      <GridWrapper posts={filteredPosts}></GridWrapper>
+      <Categories handleClick={setFilterableCategories} categories={categories}></Categories>
+    </section>
+  );
+};
+
+export default FilterablePosts;
