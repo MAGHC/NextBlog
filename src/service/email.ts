deb920f6e80f2ff2f80597817d6fab907823f51f
MAGHC <windowsapple95@gmail.com>
2023-05-09 22:06:22 +0900

    feat: nodemailer sendEmail 구현

--- src/service/email.ts
+++ src/service/email.ts
@@ -1,5 +1,31 @@
+import nodemailer from 'nodemailer';
+
 export type EmailData = {
   from: string;
   subject: string;
   message: string;
 };
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.USER, // generated ethereal user
+    pass: process.env.PASS, // generated ethereal password
+  },
+});
+export async function sendEmail({ subject, from, message }: EmailData) {
+  const mailData = {
+    to: process.env.USER,
+    subject: `[BLOG] ${subject}`,
+    from,
+    html: `
+    <h1>${subject}</h1>
+    <div>${message}</div>
+    <br/>
+    <p>보낸사람 :${from}</p>`,
+  };
+
+  return transporter.sendMail(mailData);
+}
