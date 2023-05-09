70d8caed1f296bc315f8420479a02902b8b5ed60
MAGHC <windowsapple95@gmail.com>
2023-05-09 21:52:11 +0900

    service: sendEmail 통신 추상화

---
+++ src/service/contact.ts
@@ -0,0 +1,15 @@
+import { EmailData } from './email';
+
+export async function sendEmail(emailData: EmailData) {
+  const res = await fetch('api/conatct', {
+    method: 'POST',
+    body: JSON.stringify(emailData),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const data = await res.json();
+  if (!res.ok) throw new Error('something went wrong/500');
+  if (res.ok) return data;
+}
